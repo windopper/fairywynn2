23380ff08d4849586b9b9f889518df94751294ce
windopper <open0916@naver.com>
2022-10-18 13:45:05 +0000

    add marked

--- src/components/postwrite/PostWriteHeader.vue
+++ src/components/postwrite/PostWriteHeader.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>Header</div>
+  <div class="header-container">
+    <button @click="$_setToMarkDowned">parse to markdown</button>
+    <button @click="$_setToRaw">parse to raw</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PostWriteHeader"
+  name: "PostWriteHeader",
+  props: {
+    setShowingContentType: Function,
+    showingContentType: String
+  },
+  methods: {
+    $_setToRaw: function() {
+      this.setShowingContentType("RAW");
+    },
+    $_setToMarkDowned: function() {
+      this.setShowingContentType("MARKDOWNED");
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.header-container {
+  margin: 1rem;
+  width: 100%;
+  font-size: 3rem;
+  padding: 1rem 0.5rem;
+}
+</style>
--- src/views/PostWriteView.vue
+++ src/views/PostWriteView.vue
@@ -1,16 +1,41 @@
 <template>
-  <PostWriteHeader />
-  <PostWriteBody />
+  <PostWriteHeader
+    :setShowingContentType="setShowingContentType"
+    :showingContentType="showingContentType"
+  />
+  <PostWriteBody
+    v-model:content="content"
+    :markDownedContent="markDownedContent"
+    :showingContentType="showingContentType"
+  />
 </template>
 
 <script>
 import PostWriteBody from "../components/postwrite/PostWriteBody.vue";
 import PostWriteHeader from "../components/postwrite/PostWriteHeader.vue";
+import { marked } from "marked";
 export default {
   name: "PostWriteView",
+  data() {
+    return {
+      title: "",
+      content: "",
+      markDownedContent: "",
+      showingContentType: "RAW" // RAW, MARKDOWNED
+    };
+  },
   components: {
     PostWriteBody,
     PostWriteHeader
+  },
+  methods: {
+    setShowingContentType: function(type) {
+      this.showingContentType = type;
+      this.markDownedContent = this.parseToMarkDown(this.content);
+    },
+    parseToMarkDown: function(value) {
+      return marked.parse(value);
+    }
   }
 };
 </script>
